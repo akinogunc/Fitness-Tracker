e842f0cfb159493c56441b26099be2dee3c98b54
Maruf Nebil Ogunc <home@marufs-imac.local>
2018-07-03 19:56:15 +0300

    Added workout screen class to a new branch

---
+++ Fitness Tracker/WorkoutViewController.h
@@ -0,0 +1,13 @@
+//
+//  WorkoutViewController.h
+//  Fitness Tracker
+//
+//  Created by Maruf Nebil Ogunc on 3.07.2018.
+//  Copyright © 2018 Maruf Nebil Ogunc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WorkoutViewController : UIViewController
+
+@end
---
+++ Fitness Tracker/WorkoutViewController.m
@@ -0,0 +1,37 @@
+//
+//  WorkoutViewController.m
+//  Fitness Tracker
+//
+//  Created by Maruf Nebil Ogunc on 3.07.2018.
+//  Copyright © 2018 Maruf Nebil Ogunc. All rights reserved.
+//
+
+#import "WorkoutViewController.h"
+
+@interface WorkoutViewController ()
+
+@end
+
+@implementation WorkoutViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
