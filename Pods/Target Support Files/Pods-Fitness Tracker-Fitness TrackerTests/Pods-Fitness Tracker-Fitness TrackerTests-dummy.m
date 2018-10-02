69a374636f8d59da974679609d76c7ea67ddfa4a
Maruf Nebil Ogunc <home@marufs-imac.local>
2018-10-02 22:29:04 +0300

    pods update

---
+++ Pods/Target Support Files/Pods-Fitness Tracker-Fitness TrackerTests/Pods-Fitness Tracker-Fitness TrackerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fitness_Tracker_Fitness_TrackerTests : NSObject
+@end
+@implementation PodsDummy_Pods_Fitness_Tracker_Fitness_TrackerTests
+@end
---
+++ Pods/Target Support Files/xRadioButton/xRadioButton-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_xRadioButton : NSObject
+@end
+@implementation PodsDummy_xRadioButton
+@end
